a9f7db67f4d8188d007e95602748ee3aaaa7cb13
graygabrielle <gray.atkinson.adams@gmail.com>
2019-07-17 22:13:12 -0700

    created user model and added user registration to database on user post route

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get("/", (req, res) =>
 // Connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 //Define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
