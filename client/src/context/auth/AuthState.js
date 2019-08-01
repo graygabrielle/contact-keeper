845f2b7a24535422b470afdf2be9441e013d7459
graygabrielle <gray.atkinson.adams@gmail.com>
2019-07-31 23:27:29 -0700

    conditional render of links on navbar depending on if logged in

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -94,6 +94,7 @@ const AuthState = props => {
   };
 
   //logout
+  const logout = () => dispatch({ type: LOGOUT });
 
   //clear errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
@@ -109,7 +110,8 @@ const AuthState = props => {
         register,
         clearErrors,
         loadUser,
-        login
+        login,
+        logout
       }}
     >
       {props.children}
